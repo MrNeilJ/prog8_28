5cac5746c0d5bd2c6afb922e873d0d659d034110
Neil Johnson <mr.neil.j@gmail.com>
2017-02-16 09:55:42 -0800

    Added in the first third of the application.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
 // This program demonstrates the vector size,
 // push_back, and pop_back member functions
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> values;
+
+    // Store values in the vector
+    values.push_back(1);
+    values.push_back(2);
+    values.push_back(3);
+
+    cout << "The size of values is " << values.size() << endl;
+}
